6257452cbec039cdf07bf36449413268dc3a2b06
elyas-benyoub <embenyoub@laplateforme.io>
2025-07-24 10:46:05 +0200

    jour06/job05

---
+++ jour06/job05/index.php
@@ -0,0 +1,12 @@
+<?php
+var_dump($_GET);
+?>
+
+<form action="index.php">
+    <select name="color" id="color">
+        <option value="style1">red</option>
+        <option value="style2">blue</option>
+        <option value="style3">green</option>
+    </select>
+    <button type="submit">Changer</button>
+</form>
