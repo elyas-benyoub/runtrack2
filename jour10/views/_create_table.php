a91c79265e8e125fd2c5bbf74f0c35d227820046
elyas-benyoub <embenyoub@laplateforme.io>
2025-08-02 13:34:10 +0200

    Test plesk special chars

--- jour10/views/_create_table.php
+++ jour10/views/_create_table.php
@@ -1,18 +1,27 @@
-<table border="1">
-    <thead>
-        <tr>
-            <?php foreach ($fields as $field): ?>
-                <th><?= $field->name ?></th>
-            <?php endforeach ?>
-        </tr>
-    </thead>
-    <tbody>
-        <?php while ($row = $result->fetch_assoc()): ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>RunTrack2</title>
+</head>
+<body>
+    <table border="1">
+        <thead>
             <tr>
-                <?php foreach ($row as $data): ?>
-                    <td><?= $data ?></td>
+                <?php foreach ($fields as $field): ?>
+                    <th><?= $field->name ?></th>
                 <?php endforeach ?>
             </tr>
-        <?php endwhile ?>
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            <?php while ($row = $result->fetch_assoc()): ?>
+                <tr>
+                    <?php foreach ($row as $data): ?>
+                        <td><?= $data ?></td>
+                    <?php endforeach ?>
+                </tr>
+            <?php endwhile ?>
+        </tbody>
+    </table>
+</body>
+</html>
